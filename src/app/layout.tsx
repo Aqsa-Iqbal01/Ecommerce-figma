0d5cb84ef3fcad9ca6b00c242be1be8d33871716
Aqsa Iqbal <164233800+Aqsa-Iqbal01@users.noreply.github.com>
2024-12-07 22:23:24 +0500

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
-import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Hero from "./components/Hero";
+
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -25,7 +24,6 @@ export default function RootLayout({
       <body className={inter.className}>
 
       <Header/>
-        {/* <Navbar/> */}
        
        
         {children}</body>
